0f719fad8324be87739e1a9788c3d619c58f4d12
Yota Nagahata <nagahata-yota@c-fo.com>
2017-10-23 11:31:57 +0900

    Add methods for calculation

--- date.go
+++ date.go
@@ -18,7 +18,7 @@ type Date struct {
 // Today returns Date of today
 func Today() Date {
 	return Date{
-		t: time.Now(),
+		time.Now(),
 	}
 }
 
@@ -29,7 +29,7 @@ func Parse(layout, value string) (Date, error) {
 		return Date{}, err
 	}
 
-	return Date{t: p}, nil
+	return Date{p}, nil
 }
 
 // Format is wrapper of Format method of time.Time
@@ -42,28 +42,25 @@ func (d Date) String() string {
 	return d.Format(RFC3339)
 }
 
-// Equal returns bool resulted from comparing two date object
+// Equal reports whether d and date represent the same date instant.
 func (d Date) Equal(date Date) bool {
-	year, month, day := d.t.Date()
-	xyear, xmonth, xday := date.t.Date()
-
-	return year == xyear && month == xmonth && day == xday
+	return d.t.Year() == date.t.Year() &&
+		d.t.Month() == date.t.Month() &&
+		d.t.Day() == date.t.Day()
 }
 
-// After reports whether the time instant d is after date.
+// After reports whether the date instant d is after date.
 func (d Date) After(date Date) bool {
-	year, month, day := d.t.Date()
-	xyear, xmonth, xday := date.t.Date()
-
-	return year >= xyear && month >= xmonth && day > xday
+	return d.t.Year() >= date.t.Year() &&
+		d.t.Month() >= date.t.Month() &&
+		d.t.Day() > date.t.Day()
 }
 
-// Before reports whether the time instant d is before date.
+// Before reports whether the date instant d is before date.
 func (d Date) Before(date Date) bool {
-	year, month, day := d.t.Date()
-	xyear, xmonth, xday := date.t.Date()
-
-	return year <= xyear && month <= xmonth && day < xday
+	return d.t.Year() <= date.t.Year() &&
+		d.t.Month() <= date.t.Month() &&
+		d.t.Day() < date.t.Day()
 }
 
 // YearDay is wrapper of YearDay method of time.Time
@@ -75,4 +72,71 @@ func (d Date) YearDay() int {
 // January 1, year 1, 00:00:00 UTC.
 func (d Date) IsZero() bool {
 	return d.t.IsZero()
+}
+
+// Year returns year of date
+func (d Date) Year() int {
+	return d.t.Year()
+}
+
+// Month returns month of date
+func (d Date) Month() time.Month {
+	return d.t.Month()
+}
+
+// Day returns day of date
+func (d Date) Day() int {
+	return d.t.Day()
+}
+
+// Add year, month, day to d
+func (d Date) Add(years, months, days int) Date {
+	return Date{d.t.AddDate(years, months, days)}
+}
+
+func (d Date) Diff(date Date) int64 {
+	dyear := d.Year()
+	dateyear := date.Year()
+	diffYearDay := int64(date.YearDay() - d.YearDay())
+
+	if dyear == dateyear {
+		return diffYearDay
+	}
+
+	var diff int64
+	if dyear < dateyear {
+		for y := dateyear; dyear <= y; y-- {
+			if y == dyear {
+				diff += diffYearDay
+			} else {
+				diff += int64(baseDaysInYear(y))
+			}
+		}
+	} else {
+		for y := dateyear; y <= dyear; y++ {
+			if y == dyear {
+				diff += diffYearDay
+			} else {
+				diff += int64(baseDaysInYear(y))
+			}
+		}
+	}
+	return diff
+}
+
+const (
+	daysInBasicYear int = 365
+	daysInLeapYear  int = 366
+)
+
+func baseDaysInYear(year int) int {
+	if isLeapYear(year) {
+		return daysInLeapYear
+	}
+	return daysInBasicYear
+}
+
+// isLeap is implemented in time package
+func isLeapYear(year int) bool {
+	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
 }
