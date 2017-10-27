33bb802e611d262def4fd10e9de238f48ebca55a
Yota Nagahata <nagahata-yota@c-fo.com>
2017-10-27 10:38:36 +0900

    Add null test

--- null_test.go
+++ null_test.go
@@ -1,19 +1,79 @@
 package godate
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+	"time"
+)
 
 func TestNewNullDate(t *testing.T) {
+	nd := NewNullDate(New(2017, 10, 27, time.UTC), true)
+	if !nd.Valid {
+		t.Errorf("expected valid, but invalid")
+	}
+	if nd.Date.String() != "2017-10-27" {
+		t.Errorf("expected '2017-10-27', but got %s", nd.Date.String())
+	}
 
+	nd = NewNullDate(Date{}, false)
+	if nd.Valid {
+		t.Errorf("expected invalid, but valid")
+	}
+	if !nd.Date.IsZero() {
+		t.Errorf("expected date is zero, but not zero")
+	}
 }
 
 func TestNullDateFrom(t *testing.T) {
+	nd := NullDateFrom(New(2017, 10, 27, time.UTC))
+	if !nd.Valid {
+		t.Errorf("expected valid, but invalid")
+	}
+	if nd.Date.String() != "2017-10-27" {
+		t.Errorf("expected '2017-10-27' returned, but got %s", nd.Date.String())
+	}
+}
 
+func TestNullDateFromPtr(t *testing.T) {
+	nd := NullDateFromPtr(&New(2017, 10, 27, time.UTC))
+	if !nd.Valid {
+		t.Errorf("expected valid, but invalid")
+	}
+	if nd.Date.String() != "2017-10-27" {
+		t.Errorf("expected '2017-10-27' returned, but got %s", nd.Date.String())
+	}
+
+	nd = NullDateFromPtr(nil)
+	if nd.Valid {
+		t.Errorf("expected invalid, but valid")
+	}
+	if !nd.Date.IsZero() {
+		t.Errorf("expected date is zero, but not zero")
+	}
 }
 
 func TestNullDate_SetValid(t *testing.T) {
+	nd := NullDateFromPtr(nil)
+	if nd.Valid {
+		t.Fatal("unexpected nd.Valid is true")
+	}
 
+	nd.SetValid(New(2017, 10, 27, time.UTC))
+	if !nd.Valid {
+		t.Errorf("expected valid, but invalid")
+	}
+	if nd.Date.String() != "2017-10-27" {
+		t.Errorf("expected '2017-10-27' returned, but got %s", nd.Date.String())
+	}
 }
 
 func TestNullDate_Ptr(t *testing.T) {
+	d := New(2017, 10, 27, time.UTC)
+	nd := NullDateFrom(d)
+	dp := nd.Ptr()
 
+	v := reflect.ValueOf(dp)
+	if v.Kind() != reflect.Ptr {
+		t.Errorf("expected Ptr returns pointer, but got %v", v.Kind())
+	}
 }
